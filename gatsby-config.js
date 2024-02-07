e9aa2b6ac2e78afde02af5bfd203e9e40fb6df5b
David Riches <hi@davidrich.es>
2024-02-07 21:46:39 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,28 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Hurkle Huts`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "yVZbmylJwmZQHHFfgknQsS75tEW9AhnJ2q4F9mkx_6E",
+      "spaceId": "pcd8jecof9o4"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss", "gatsby-plugin-google-gtag", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
