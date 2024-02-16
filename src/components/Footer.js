0b9b11fc5a3e7a1086985e7429a06d3c07c18cb1
David Riches <hi@davidrich.es>
2024-02-16 17:11:45 +0000

    updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,29 @@
+import { Link } from 'gatsby';
 import React from 'react';
+import { FaInstagram } from 'react-icons/fa';
 
 const Footer = () => (
   <footer className='text-center font-sans'>
-    <div className='p-24 bg-secondary text-white'>
-      This is a footer
-      <div className='text-primary-bistre'>Social links to go here?</div>
+    <div className='p-16 bg-secondary text-white'>
+      Contact us
+      <div className='text-primary-bistre'>
+        <div>0800 123 4567</div>
+        <div>
+          <Link to='mailto:info@hurkle.co.uk'>info@hurkle.co.uk</Link>
+        </div>
+      </div>
+      <div className='text-primary-bistre'>
+        <div className='flex flex-row justify-center pt-4'>
+          <Link to='https://www.instagram.com/hurkle.huts/'>
+            <div className='flex flex-row items-center gap-1'>
+              <FaInstagram /> Follow us on Instagram
+            </div>
+          </Link>
+        </div>
+      </div>
     </div>
-    <div className='bg-black p-12 text-white'>Contact number etc</div>
+    {/* <div className='bg-black p-12 text-white'>
+    </div> */}
   </footer>
 );
 
