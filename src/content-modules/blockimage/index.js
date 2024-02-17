5d30f5fd324cc44e88e58569a8833d3f21027194
David Riches <hi@davidrich.es>
2024-02-17 19:36:02 +0000

    contact form tweaks

--- src/content-modules/blockimage/index.js
+++ src/content-modules/blockimage/index.js
@@ -9,7 +9,7 @@ export default function BlockImage({ image, hideBlockTitle, title }) {
           <div className='absolute text-white drop-shadow-xl text-center z-[2] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
             <h3 className='text-center p-2'>{title}</h3>
           </div>
-          <div className='absolute z-[1] top-0 right-0 bg-gradient-to-t from-[#231f20] h-full w-full' />
+          <div className='absolute z-[1] top-0 right-0 bg-gradient-to-b from-[#231f20] h-full w-full' />
         </>
       )}
       <GatsbyImage
